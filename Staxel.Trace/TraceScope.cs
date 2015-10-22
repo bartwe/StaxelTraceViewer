a86ebccf5c478c5d76d3e4e3385e0f68f22f4903
Bart van der Werf <bluelive@gmail.com>
2015-10-22 22:24:47 +0200

    Patch out the Tracescope for as far as possible when applicable.

--- Staxel.Trace/TraceScope.cs
+++ Staxel.Trace/TraceScope.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime;
+using System.Runtime.CompilerServices;
 
 namespace Staxel.Trace {
     public struct TraceScope : IDisposable {
         public TraceKey Key;
 
+        [TargetedPatchingOptOut("")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TraceScope(TraceKey key) {
             Key = key;
             TraceRecorder.Enter(Key);
         }
 
+        [Conditional("TRACE")]
+        [TargetedPatchingOptOut("")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Dispose() {
             TraceRecorder.Leave(Key);
         }
