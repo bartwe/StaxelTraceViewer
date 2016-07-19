911a43deacfb16660870a727de5bd210554713ba
bartwe <bluelive@gmail.com>
2016-07-19 13:15:36 +0200

    autocleanup

--- Staxel.Trace/TraceRecorder.cs
+++ Staxel.Trace/TraceRecorder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Runtime;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
